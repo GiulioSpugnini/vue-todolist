24df3f9d88ee5400a0cc7b79c7cd0ae23062c4d3
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-28 15:26:06 +0100

    created a list of object and linked it in a ul on html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
 console.log('Vue OK', Vue);
 Vue.config.devtools = true;
+
+const app = new Vue({
+    el: ' #root',
+    data: {
+        items: [{
+                text: 'Carne',
+                done: false,
+            },
+            {
+                text: 'Latte',
+                done: false,
+            },
+            {
+                text: 'Pesce',
+                done: false,
+            },
+            {
+                text: 'Acqua',
+                done: false,
+            },
+            {
+                text: 'Pane',
+                done: false,
+            },
+            {
+                text: 'Cioccolate',
+                done: false,
+            },
+        ]
+    },
+});
